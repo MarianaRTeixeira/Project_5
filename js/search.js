3fdbb9adc4950cc4e919c24b8858927fe37a2349
MarianaRTeixiera <marianamrochateixeira@gmail.com>
2020-12-04 15:35:33 +0000

    search

--- js/search.js
+++ js/search.js
@@ -1,19 +1,26 @@
-function myFunction() {
-
-    var input;
+function mySearchEng() {
+    let input;
+    let filter;
+    let gallery;
+    let img;
     
-    input = document.getElementById("myInput");
 
+    input = document.getElementById('myInput');
     filter = input.value.toUpperCase();
-    caption = document.getElementById("data-caption");
-    li = ul.getElementsByTagName("data-caption"); //change
-    for (i = 0; i < li.length; i++) {
-        a = li[i].getElementsByTagName("a")[0];
-        txtValue = a.textContent || a.innerText;
-        if (txtValue.toUpperCase().indexOf(filter) > -1) {
-            li[i].style.display = "";
-        } else {
-            li[i].style.display = "none";
-        }
-    }
-}
+
+    gallery = document.getElementById('gallery');
+    img = gallery.getElementsByTagName('a');
+  
+    let caption;
+
+   for (let i = 0; i < img.length; i++ ) {
+       caption = img[i].getAttribute('data-caption');
+       if(caption.toUpperCase().includes(filter)){
+           img[i].style.display= "";
+       } else {
+           img[i].style.display= "none";
+       }
+   }
+
+}    
+//w3school
